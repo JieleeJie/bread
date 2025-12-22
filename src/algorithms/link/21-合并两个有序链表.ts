39b16dd9501d10c84132f002b8e9d7da0a8c345a
lijie602 <lijie602@jd.com>
2025-12-22 17:34:58 +0800

    feat

---
+++ src/algorithms/link/21-合并两个有序链表.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  const dummy = new ListNode();
+  let p = dummy;
+  let p1 = list1;
+  let p2 = list2;
+  while (p1 && p2) {
+    if (p1.val < p2.val) {
+      p.next = p1;
+      p1 = p1.next;
+    } else {
+      p.next = p2;
+      p2 = p2.next;
+    }
+    p = p.next;
+  }
+
+  if (p1) {
+    p.next = p1;
+  }
+  if (p2) {
+    p.next = p2;
+  }
+
+  return dummy.next;
+}
---
+++ src/algorithms/link/86-分隔链表.ts
@@ -0,0 +1,63 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function partition(head: ListNode | null, x: number): ListNode | null {
+  // 存放小于 x 的链表的虚拟头结点
+  const dummy1 = new ListNode(-1);
+  // 存放大于 x 的链表的虚拟头结点
+  const dummy2 = new ListNode(-1);
+  // 指针
+  let p1 = dummy1;
+  let p2 = dummy2;
+  let p = head;
+
+  while (p !== null) {
+    if (p.val < x) {
+      p1.next = p;
+      p1 = p1.next;
+    } else {
+      p2.next = p;
+      p2 = p2.next;
+    }
+    // 断开原连接的 next 指针。否则 p = p.next 直接前进会形成环
+    const temp = p?.next;
+    p.next = null;
+    p = temp;
+  }
+
+  p1.next = dummy2.next;
+  return dummy1.next;
+}
+
+function partition2(head: ListNode | null, x: number): ListNode | null {
+  // 存放小于 x 的链表的虚拟头结点
+  const dummy1 = new ListNode(-1);
+  // 存放大于 x 的链表的虚拟头结点
+  const dummy2 = new ListNode(-1);
+  // 指针
+  let p1 = dummy1;
+  let p2 = dummy2;
+  let p = head;
+
+  while (p !== null) {
+    if (p.val < x) {
+      p1.next = p;
+      p1 = p1.next;
+    } else {
+      p2.next = p;
+      p2 = p2.next;
+    }
+    p = p.next;
+  }
+  p1.next = null;
+  p2.next = null;
+
+  p1.next = dummy2.next;
+  return dummy1.next;
+}
