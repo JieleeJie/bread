571d6cebbff73a2b1f4ab9866f9e97696edc3c58
lijie602 <lijie602@jd.com>
2026-02-09 20:57:00 +0800

    feat: 盛最多水的容器

---
+++ src/algorithms/arr/相向双指针/习题-125. 验证回文串.ts
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.cn/problems/valid-palindrome/description/
+ */
+function isPalindrome(s: string): boolean {
+  const str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  // 纯库方法
+  // return str === str.split('').reverse().join('');
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
