a78190a36dde8c3d108cbef224f4e5ac2e57df16
jieleejie <2260068153@qq.com>
2026-03-29 14:45:48 +0800

    feat: 1. 两数之和

---
+++ src/algorithms/hot-100/1. 两数之和.ts
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+ * 遍历一遍，遍历时把已经遍历过的元素存在 map 中，再看 map 中是不是有满足 target - element 的元素
+ */
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i]!;
+    if (map.has(target - element)) {
+      return [i, map.get(target - element)];
+    }
+    map.set(element, i);
+  }
+}
