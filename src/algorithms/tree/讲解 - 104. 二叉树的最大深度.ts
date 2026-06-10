85b64c46a354239f8c2448d5b1496ca078a9344d
lijie602 <lijie602@jd.com>
2026-06-10 11:07:06 +0800

    feat: rename

---
+++ src/algorithms/tree/讲解 - 104. 二叉树的最大深度.ts
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+ */
+
+import TreeNode from '../../TreeNode';
+function maxDepth(root: TreeNode | null): number {
+  if (!root || root?.val === null) {
+    return 0;
+  }
+  const leftMaxDepth = maxDepth(root?.left);
+  const rightMaxDepth = maxDepth(root?.right);
+
+  return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+}
+
+const node3 = new TreeNode(3);
+const node4 = new TreeNode(4);
+const node5 = new TreeNode(5);
+node3.left = node4;
+node3.right = node5;
