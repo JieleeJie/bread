5be313aa21d17b94e369c9e6dae52690efbf51c7
lijie602 <lijie602@jd.com>
2026-06-10 14:01:08 +0800

    feat: 习题- 404. 左叶子之和

---
+++ src/algorithms/tree/习题- 404. 左叶子之和.ts
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.cn/problems/sum-of-left-leaves/description/
+ */
+
+import TreeNode from '../../TreeNode';
+function sumOfLeftLeaves(root: TreeNode | null): number {
+  let answer = 0;
+
+  function dep(node: TreeNode | null, depth?: number) {
+    if (!node) {
+      return;
+    }
+    // 这只能判断是叶子节点，不能判断是左叶子
+    // if (node && node.left === null && node.right === null) {
+    //   answer += node.val;
+    // }
+
+    const left = node.left; // 当前节点的左儿子
+    if (left && left.left === null && left.right === null) {
+      // 当前节点的左儿子是叶子
+      answer += left.val; // 累加节点值
+    }
+    dep(node.left);
+    dep(node.right);
+  }
+
+  dep(root);
+
+  return answer;
+}
